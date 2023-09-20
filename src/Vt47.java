cff94a045949edf0570b16c636c0ef41e0c5287d
Ashifa09 <ishafaridi8@gmail.com>
2023-09-20 15:27:10 +0530

    Changes in vt47

--- src/Vt47.java
+++ src/Vt47.java
@@ -4,5 +4,8 @@ public class Vt47 {
     max nodes=2^i
     wAP to create a binary tree using ll
     */
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
     
 }
